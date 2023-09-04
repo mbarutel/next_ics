39db28dc64a173c39aef3d30b7de4d8385643241
mike-ninja <mikeedubas@hotmail.com>
2023-09-04 13:38:08 +0300

    setting up carousel

--- components/home/Advantages.tsx
+++ components/home/Advantages.tsx
@@ -1,28 +1,23 @@
 import React, { Fragment } from "react";
-import { BiSolidRightArrow } from "react-icons/bi";
-import { cta } from "@/lib/data";
+import { advantages } from "@/lib/data";
 
 export default function Advantages() {
   return (
-    <section className="relative bg-advantages_background bg-cover bg-center">
-      <div className="container">
-        <h2 className="flex justify-center mb-4">
-          Become&nbsp;part&nbsp;of&nbsp;ICS
-        </h2>
-        <div className="bg-off_white rounded-xl p-5 grid grid-cols-1 gap-3 shadow">
-          {cta.map((item, index) => (
-            <Fragment key={index}>
-              <div className="flex text-light_brown items-center md:justify-center mb-1 md:mb-2 text-xl md:text-3xl">
-                <BiSolidRightArrow className="hidden md:block mr-1 text-lg" />
-                <h3>{item.header}</h3>
-              </div>
-              {item.description.map((description, index) => (
-                <Fragment key={index}>
-                  <p className="mb-1 text-left md:text-center">{description}</p>
-                </Fragment>
-              ))}
-            </Fragment>
-          ))}
+    <section>
+      <div className="container relative bg-advantages_background bg-cover bg-center bg-fixed rounded-xl py-7">
+        <div className="bg-white/40 rounded-xl p-5 grid grid-cols-1 gap-3 shadow min-w-[50%]">
+          <h2 className="section_header text-stone-600/80 text-center">
+            Become&nbsp;part&nbsp;of&nbsp;ICS
+          </h2>
+          <div className="grid grid-cols-2 gap-3">
+            {advantages.map((advantage, index) => (
+              <Fragment key={index}>
+                <h3 className="capitalize font-semibold text-stone-500/90 text-center">
+                  {advantage}
+                </h3>
+              </Fragment>
+            ))}
+          </div>
         </div>
       </div>
     </section>
