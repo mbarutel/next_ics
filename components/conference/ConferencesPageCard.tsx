1d76031a13ed6d053b5dec97ee88d4e72067c729
mike-ninja <mikeedubas@hotmail.com>
2023-10-09 19:50:40 +0300

    fixed small device conferences page

--- components/conference/ConferencesPageCard.tsx
+++ components/conference/ConferencesPageCard.tsx
@@ -17,33 +17,33 @@ export default function ConferencesPageCard(
       href={`/conferences/${conferencePage.slug}`}
       target="_blank"
       rel="noreferrer"
-      className="bg-red-800/80 grid grid-cols-2 group transition-all hover:scale-105 active:scale-95 rounded-lg shadow-xl"
+      className="bg-red-800/80 grid grid-cols-1 sm:grid-cols-2 group transition-all hover:scale-105 active:scale-95 sm:rounded-lg shadow-xl min-h-[25rem]"
     >
-      <div className="relative group-even:order-1">
+      <div className="relative sm:group-even:order-1">
         <Image
           src={conferencePage.coverImage.src}
           alt={conferencePage.coverImage.alt}
           width={conferencePage.coverImage.width}
           height={conferencePage.coverImage.height}
-          className="h-36 sm:h-[450px] object-cover shadow-lg contrast-75 group-odd:rounded-l-lg group-even:rounded-r-lg"
+          className="h-full object-cover shadow-lg contrast-75 sm:group-odd:rounded-l-lg sm:group-even:rounded-r-lg"
         />
       </div>
-      <div className="flex flex-col justify-center items-center text-center py-6">
+      <div className="flex flex-col justify-center items-center text-center py-6 px-4">
         <h4 className="text-white font-black leading-none text-lg sm:text-2xl uppercase">
           {conferencePage.title}
         </h4>
         <span className="flex gap-3">
-          <span className="text-base sm:text-lg font-normal text-white">
+          <span className="text-base lg:text-lg font-normal text-white">
             🗓️&nbsp;
             {dayjs(conferencePage.startDate).format("DD")} -{" "}
             {dayjs(conferencePage.endDate).format("DD MMM, YYYY")}
           </span>
-          <span className="capitalize text-base sm:text-lg font-normal text-white">
+          <span className="capitalize text-base lg:text-lg font-normal text-white">
             📍&nbsp;
             {conferencePage.venueName}
           </span>
         </span>
-        <p className="px-8 mt-2 text-justify text-white">
+        <p className="mt-2 text-justify text-white">
           {conferencePage.description}
         </p>
       </div>
