a752f0dc2f3f07457554f7df901ddad9b04a159b
mike-ninja <mikeedubas@hotmail.com>
2023-10-25 17:35:59 +0300

    update bullet points

--- components/rich-text/list-item.tsx
+++ components/rich-text/list-item.tsx
@@ -1,10 +1,9 @@
 import React, { ReactNode } from "react";
-import { GoDotFill } from "react-icons/go";
 
 export default function ListItem({ children }: { children: ReactNode }) {
   return (
-    <li className="flex items-start text-sm sm:text-base">
-      <GoDotFill className="text-slate-800/80 text-xs min-w-fit mr-1 mb-2 mt-1 sm:mt-[6px] lg:mt-[7px]" />
+    <li className="text-sm sm:text-base flex gap-2">
+      •
       {children}
     </li>
   );
