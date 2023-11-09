98a3f6d1a7e98b752ac61c068b5a5f07896feec9
mike-ninja <mikeedubas@hotmail.com>
2023-11-09 12:52:31 +0200

    updated lib/data and added new exhibitor form link

--- lib/data.ts
+++ lib/data.ts
@@ -4,20 +4,11 @@ export const configs = {
     email: "admin@icsconferences.org",
     address: "13 Seaford Entrance, Kewarra Beach QLD 4879",
   },
-  defaultCoverImage: {
-    src:
-      "https://images.unsplash.com/photo-1540575467063-178a50c2df87?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
-    alt: "default-cover-image",
-    width: 192,
-    height: 192,
-  },
   forms: {
-    registration:
-      "https://docs.google.com/forms/d/e/1FAIpQLSe-8xH9Ee8FvO5uF_Uq2bRYa5lIdAEN8buDzHMuElDW_DvkAw/viewform",
     submitPaper:
       "https://docs.google.com/forms/d/e/1FAIpQLScR8xcw0IrdVy60HHTD2ECZijXKWtyQyq3S3oEuYRw8m33XWA/viewform",
     exhibitor:
-      "https://forms.office.com/pages/responsepage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAa__ZvvzOdURU5XVUZNTk1KVzRCSUM3VVc0OTM0VVYyUC4u",
+      "https://docs.google.com/forms/d/e/1FAIpQLScRpsjKM0uqOmpJjurxMQZbz2o37_F_Gm9aR7PAXS9J-9U3tw/viewform",
     sponsor:
       "https://forms.office.com/pages/responsepage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAa__ZvvzOdURDE3WU0yN0M2VzRCQzNTMEVYUkdKRlBTVi4u",
   },
@@ -30,8 +21,7 @@ export const cta = [
       "Empower others with your stories, insights, and wisdom, inspiring a world where Indigenous culture is celebrated and respected.",
     button: "Become a Speaker",
     image: "/assets/images/advantages-two.webp",
-    form:
-      "https://forms.office.com/pages/responsepage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAa__ZvvzOdURDA4VDFPTlgzVU01OUtBM0pCSkRBWFU0OS4u",
+    form: configs.forms.submitPaper,
   },
   {
     title: "Exhibitions",
@@ -39,8 +29,7 @@ export const cta = [
       "Use your platform to create awareness about vital Indigenous causes and issues, inspiring attendees to become advocates for positive change.",
     button: "Become an Exhibitor",
     image: "/assets/images/advantages-three.webp",
-    form:
-      "https://forms.office.com/pages/responsepage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAa__ZvvzOdURU5XVUZNTk1KVzRCSUM3VVc0OTM0VVYyUC4u",
+    form: configs.forms.exhibitor,
   },
   {
     title: "Sponsorship",
@@ -48,8 +37,7 @@ export const cta = [
       "Associate your brand with a cause that matters, gaining valuable exposure to a diverse and engaged audience, both locally and globally.",
     button: "Become a Sponsor",
     image: "/assets/images/advantages-one.webp",
-    form:
-      "https://forms.office.com/pages/responsepage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAa__ZvvzOdURDE3WU0yN0M2VzRCQzNTMEVYUkdKRlBTVi4u",
+    form: configs.forms.sponsor,
   },
 ] as const;
 
