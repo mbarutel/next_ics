909705ccdd9764f2be331c07cb1f57bcda8d688d
mike-ninja <mikeedubas@hotmail.com>
2024-03-24 09:09:29 +1000

    changes to even tab button

--- components/event-information.tsx
+++ components/event-information.tsx
@@ -6,8 +6,8 @@ import { Agenda, EventText, Masterclass } from ".";
 import { AgendaType, EventType, MasterclassType } from "@/lib/types";
 
 export default function EventInformation(event: EventType) {
-  const [tab, setTab] = useState<"Information" | "Agenda" | "Masterclass">(
-    "Information",
+  const [tab, setTab] = useState<"Conference" | "Agenda" | "Masterclass">(
+    "Conference",
   );
 
   let agenda = null;
@@ -52,7 +52,7 @@ export default function EventInformation(event: EventType) {
 type TabsProps = {
   tab: string;
   setTab: React.Dispatch<
-    React.SetStateAction<"Information" | "Agenda" | "Masterclass">
+    React.SetStateAction<"Conference" | "Agenda" | "Masterclass">
   >;
   agenda: AgendaType[] | null;
   masterclass: MasterclassType[] | null;
@@ -61,17 +61,17 @@ function Tabs({ tab, setTab, agenda, masterclass }: TabsProps) {
   return (
     <div className="event_tabs_wrapper">
       <button
-        onClick={() => setTab("Information")}
+        onClick={() => setTab("Conference")}
         className={clsx(
           "event_tab_text px-3 bg-stone-800/80 font-extrabold py-3 rounded-md sm:rounded-b-none transition_config",
           {
             "bg-gradient-to-r gradient text-stone-800 rounded-md sm:rounded-b-none":
-              tab === "Information",
-              "sm:border-b-2 sm:border-stone-900 hover:-translate-y-1" : tab !== "Information",
+              tab === "Conference",
+              "sm:border-b-2 sm:border-stone-900 hover:-translate-y-1" : tab !== "Conference",
           },
         )}
       >
-        Information
+        Conference
       </button>
       {agenda && (
         <button
