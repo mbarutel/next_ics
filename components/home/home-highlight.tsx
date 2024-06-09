5fbb569c91232100f34ef588c624ca9f47bff7b5
Mike Edubas <mikeedubas@hotmail.com>
2024-06-09 23:24:56 +1000

    info change

--- components/home/home-highlight.tsx
+++ components/home/home-highlight.tsx
@@ -4,28 +4,30 @@ export default function HomeHighlight() {
       <div className="section_container">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 text-center cursor-default">
           <div className="p-6">
-            <h2 className="text-3xl md:text-5xl lg:text-6xl font-bold">500+</h2>
+            <h2 className="text-3xl md:text-5xl lg:text-6xl font-bold">
+              20,000+
+            </h2>
             <h3 className="mt-2 text-xl md:text-2xl font-semibold text-yellow-400">
               Delegates
             </h3>
           </div>
 
           <div className="p-6">
-            <h2 className="text-3xl md:text-5xl lg:text-6xl font-bold">100+</h2>
+            <h2 className="text-3xl md:text-5xl lg:text-6xl font-bold">600+</h2>
             <h3 className="mt-2 text-xl md:text-2xl font-semibold text-yellow-400">
               Speakers
             </h3>
           </div>
 
           <div className="p-6">
-            <h2 className="text-3xl md:text-5xl lg:text-6xl font-bold">4+</h2>
+            <h2 className="text-3xl md:text-5xl lg:text-6xl font-bold">20+</h2>
             <h3 className="mt-2 text-xl md:text-2xl font-semibold text-yellow-400">
-              Locations
+              Host Cities
             </h3>
           </div>
 
           <div className="p-6">
-            <h2 className="text-3xl md:text-5xl lg:text-6xl font-bold">11+</h2>
+            <h2 className="text-3xl md:text-5xl lg:text-6xl font-bold">30+</h2>
             <h3 className="mt-2 text-xl md:text-2xl font-semibold text-yellow-400">
               Years Running
             </h3>
