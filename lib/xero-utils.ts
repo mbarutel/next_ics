80b5f37d4769dc11b901f434e1ec36cb16d274f1
Mike Edubas <mikeedubas@hotmail.com>
2024-09-15 14:33:46 +1000

    added some notes

--- lib/xero-utils.ts
+++ lib/xero-utils.ts
@@ -3,15 +3,18 @@ import { RegistrationType } from "./types";
 import { ContactGroup, ContactGroups, Contacts, LineItem } from "xero-node";
 
 export function generateLineItems({ body }: { body: RegistrationType }) {
-  let objects: LineItem[] = [{
-    taxType: "OUTPUT",
-    accountCode: "200",
-    description: "Registration Fee",
-    quantity: body.extraParticipants === ""
-      ? 1
-      : body.extraParticipants.split("\n").length + 1,
-    unitAmount: body.priceValue,
-  }];
+  let objects: LineItem[] = [
+    {
+      taxType: "OUTPUT",
+      accountCode: "200",
+      description: "Registration Fee",
+      quantity:
+        body.extraParticipants === ""
+          ? 1
+          : body.extraParticipants.split("\n").length + 1,
+      unitAmount: body.priceValue,
+    },
+  ];
 
   if (body.dinnerParticipants !== "") {
     const quantity = body.dinnerParticipants.split("\n").length;
@@ -54,25 +57,22 @@ export function generateLineItems({ body }: { body: RegistrationType }) {
   return objects;
 }
 
-export async function contactCheck(
-  body: RegistrationType,
-) {
+export async function contactCheck(body: RegistrationType) {
+  // TODO : Ultimately we want to query a single contact
+  // rather than all of them.
   const contacts = await xero.accountingApi.getContacts("");
 
   // Check name of registration to contact.name
-  const matchedName = contacts.body.contacts &&
-    contacts.body.contacts.find((contact) =>
-      contact.name?.toLowerCase() === body.mainParticipant.name.toLowerCase()
+  const matchedName =
+    contacts.body.contacts &&
+    contacts.body.contacts.find(
+      (contact) =>
+        contact.name?.toLowerCase() === body.mainParticipant.name.toLowerCase(),
     );
 
   const name = body.mainParticipant.name.split(" ");
   const firstName = name.slice(0, name.length - 1).join(" ");
   const lastName = name[name.length - 1];
-
-  if (matchedName && matchedName.contactID) {
-    return matchedName.contactID;
-  }
-
   const contactObject = {
     contacts: [
       {
@@ -84,10 +84,25 @@ export async function contactCheck(
     ],
   };
 
-  const newContact = await xero.accountingApi.createContacts(
-    "",
-    contactObject,
-  );
+  if (matchedName && matchedName.contactID) {
+    if (matchedName.emailAddress != body.mainParticipant.email) {
+      const updatedContact = await xero.accountingApi.updateContact(
+        "",
+        matchedName.contactID,
+        contactObject,
+      );
+
+      if (!updatedContact.body.contacts) {
+        throw new Error("Error Updating Contact");
+      }
+
+      return updatedContact.body.contacts[0].contactID;
+    }
+
+    return matchedName.contactID;
+  }
+
+  const newContact = await xero.accountingApi.createContacts("", contactObject);
 
   if (!newContact.body.contacts) {
     throw new Error("Error Creating New Contact");
