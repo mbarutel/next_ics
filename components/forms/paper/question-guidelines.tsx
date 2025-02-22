551c61088335a802a4e10d9c92d3265d17261b1e
mike-ninja <mikeedubas@hotmail.com>
2025-02-23 09:22:22 +1000

    updated guidelines

--- components/forms/paper/question-guidelines.tsx
+++ components/forms/paper/question-guidelines.tsx
@@ -10,12 +10,12 @@ export default function QuestionGuidelines({
     "Papers should not contain offensive language and take into account cultural sensitivities of host country.",
     "Papers may treat the themes in a manner that contributes to further discussion of conference aims.",
     "Conference papers must be presented in the finish format not less than 60 days prior to the event.",
-    "First round of papers closing date will be close of business on 30th November with a second if required.",
     "Papers that are not chosen in the first round may be resubmitted in the second round.",
     "Papers should be submitted in Microsoft Word format.",
     "Authors of papers presented at the conference will be formally notified of their acceptance.",
     "Papers should explore ways in which the themes show up in the philosophy of the conference.",
     "All papers must be presented in a positive and informative light.",
+    "A registration fee of $850 will apply to all persons submitting papers, payable within 7 days upon notification of acceptance.",
   ];
 
   return (
